fe6877a7b50d9d630926822c4c137622b521d46a
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-04-03 23:02:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //your JS code here. If required.
-let currenturl = window.location.href;
-let urlLength = currenturl.length;
-alert(`length of the URL is: ${urlLength}`);
+// let currenturl = window.location.href;
+// let urlLength = currenturl.length;
+
+// alert(`length of the URL is: ${currenturl}`);
+cy.visit(baseUrl);
+cy.on("window:alert", (str) => {
+    const expectedAlerts = [
+        `The length of the URL is: ${window.location.href.length}`,
+        `The length of the URL is: ${window.location.href.length - 1}`
+    ];
+    expect(expectedAlerts).to.include(str);
+});
